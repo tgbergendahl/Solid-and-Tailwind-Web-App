0615395929e0c7bed0c6761180820202758b9397
tgbergendahl <tgbergendahl@outlook.com>
2023-03-27 11:50:30 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+import Navbar from './components/Navbar.jsx';
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <div className="fixed flex flex-row top-0 left-0 w-screen">
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>
+            <i>E</i>
+        </div>
+    )
+}
